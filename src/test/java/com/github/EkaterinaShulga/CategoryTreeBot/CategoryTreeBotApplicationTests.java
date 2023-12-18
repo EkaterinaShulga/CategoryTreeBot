c5b565a2b2e01d2b1d10b4919bc22bf33a5d1afa
EkaterinaShulga <102359023+EkaterinaShulga@users.noreply.github.com>
2023-12-18 09:24:14 +0300

    first commit

---
+++ src/test/java/com/github/EkaterinaShulga/CategoryTreeBot/CategoryTreeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.EkaterinaShulga.CategoryTreeBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryTreeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
